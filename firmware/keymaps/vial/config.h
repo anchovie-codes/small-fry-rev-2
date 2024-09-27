b2866d7c8e7996439267a6c8c2a51dab8d94fe8e
anchovie <littlebrother.free@gmail.com>
2024-09-27 16:58:03 -0400

    Add files via upload

---
+++ firmware/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xD8, 0x68, 0x49, 0xF1, 0xF0, 0x4E, 0x19, 0xF6}
+
+#define VIAL_UNLOCK_COMBO_ROWS {2, 2, 2}
+#define VIAL_UNLOCK_COMBO_COLS {0, 1, 13}
